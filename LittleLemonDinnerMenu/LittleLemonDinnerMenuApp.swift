92d115bb55ea4c1a0b17aaa96027b3e8db657bf0
Hamza Chaouki <hamzachaouki23@gmail.com>
2023-12-01 23:40:33 +0100

    Initial Commit

---
+++ LittleLemonDinnerMenu/LittleLemonDinnerMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonDinnerMenuApp.swift
+//  LittleLemonDinnerMenu
+//
+//  Created by Hamza Chaouki on 1/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonDinnerMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
